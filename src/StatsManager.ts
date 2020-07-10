1a717856bce7c8a25bc410cd8700d2d41e868a2c
Corentin Thomasset <corentin.thomasset74@gmail.com>
2020-07-10 23:14:44 +0200

    refactor: removed unused files

--- src/StatsManager.ts
+++
@@ -1,8 +0,0 @@
-class StatsManager {
-    public enemyKilled = 0
-    public damageGiven = 0;
-    public damageTaken = 0
-    public towerCount = 0
-}
-
-export const statsManager = new StatsManager();
